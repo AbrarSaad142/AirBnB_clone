74685d48c3f8f6abd2b98caf8d3d15ae6bd105dc
Abrar Saad <abrarsaad3572022@gmail.com>
2024-03-09 23:36:39 +0200

    5. Store first object

---
+++ console.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+import cmd
+from models.base_model import BaseModel
+from models import storage
+
+
+class HBNBCommand(cmd.Cmd):
+    """HBNBCommand class"""
+
+    prompt = "(hbnb) "
+    classes = {"BaseModel": BaseModel}
+
+    def do_EOF(self, line):
+        """Exit the program"""
+        return True
+
+    def do_quit(self, arg):
+        """Exit the program"""
+        return True
+
+    def emptyline(self):
+        """empty shouldn’t execute anything"""
+        pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id."""
+        if not arg:
+            print("** class name missing **")
+        elif arg not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            new_instance = self.classes[arg]()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """Prints the string representation
+        of an instance based on the class name a"""
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            obj_dic = storage.all()
+            if key not in obj_dic:
+                print("** no instance found **")
+            else:
+                print(obj_dic[key])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class
+        name and id (save the change into the JSON file)."""
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            obj_dic = storage.all()
+            if key not in obj_dic:
+                print("** no instance found **")
+            else:
+                del obj_dic[key]
+                storage.save()
+
+    def do_all(self, arg):
+        """: Prints all string representation of
+        all instances based or not on the class name"""
+        obj_dict = storage.all()
+        if not arg:
+            print([str(obj_dict[key]) for key in obj_dict])
+        elif arg not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            print([str(obj_dict[key])
+                   for key in obj_dict if key.startswith(arg)])
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name
+        and id by adding or updating
+        attribute (save the change into the JSON file)"""
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif len(args) == 2:
+            print("** attribute name missing **")
+        elif len(args) == 3:
+            print("** value missing **")
+        else:
+            key = args[0] + "." + args[1]
+            obj_dict = storage.all()
+            if key not in obj_dict:
+                print("** no instance found **")
+            else:
+                obj = obj_dict[key]
+                setattr(obj, args[2], args[3])
+                obj.save()
+
+
+if __name__ == "__main__":
+    """infinite loop"""
+    HBNBCommand().cmdloop()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" FileStorage that serializes instances to a JSON file and deserializes JSON
+file to instances:
+"""
+
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage():
+    """that serializes instances to a JSON
+    file and deserializes JSON file to instances"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = obj.__class__.__name__ + "." + str(obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """ serializes __objects to the JSON file (path: __file_path)"""
+        json_object = {}
+        for key in self.__objects:
+            json_object[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w') as jsonFile:
+            json.dump(json_object, jsonFile)
+
+    def reload(self):
+        """ deserializes the JSON file to __objects (only if the JSON file
+        (__file_path) exists ; otherwise, do nothing.
+        If the file doesn’t exist,
+        no exception should be raised)"""
+        try:
+            with open(self.__file_path, "r", encoding="utf-8") as jsonFile:
+                for key, value in json.load(jsonFile).items():
+                    attribute_value = eval(value['__class__'])(**value)
+                    self.__objects[key] = attribute_value
+        except FileNotFoundError:
+            pass
